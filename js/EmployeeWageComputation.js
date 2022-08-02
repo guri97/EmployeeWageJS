2b6818580397b1b748bae43fa6fa50e66aef1d8b
guri97 <105732963+guri97@users.noreply.github.com>
2022-08-02 14:17:53 +0530

    [gurpreet] UC4_CalculateWagesForMonth

--- js/EmployeeWageComputation.js
+++ js/EmployeeWageComputation.js
@@ -1,9 +1,14 @@
-///////////////////////////////UC-3///////////////////////////////
-//Refactor the Code to write a function to get work hours
+console.log("Welcome to the Employee Wage Problem");
+
+///////////////////////////////UC-4///////////////////////////////
+//Calculating Wages for a Month assuming 20 Working Days in a Month
 const IS_FULL_TIME=1;
 const IS_PART_TIME=2;
 EMP_RATE_PER_HR=20;
-empCheck=Math.floor(Math.random()*10)%3;
+const MAX_WORKING_DAYS=20;
+let empHrs =0;
+let totalEmpWage=0;
+
 function getWorkHours()
 {
     switch(empCheck)
@@ -19,8 +24,14 @@ function getWorkHours()
     default:
         console.log("UC1-Employee is Absent");
             empHrs=0;
-            return 0;
+            return empHrs;
     }
 }
-empWage=EMP_RATE_PER_HR*getWorkHours();
-console.log("UC2-Employee Wage:"+empWage);
+for(let day=1;day<=MAX_WORKING_DAYS;day++)
+{
+    empCheck = Math.floor(Math.random()*10)%3;
+    empWage=EMP_RATE_PER_HR*getWorkHours();
+    console.log("UC2-Employee Wage for day-"+day+" = "+empWage);
+    totalEmpWage+=empWage;
+}
+console.log("UC4-Total Employee Wage:"+totalEmpWage);
