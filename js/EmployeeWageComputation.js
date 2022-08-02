36b1a03fee2575da684682005dfc27aa9329e0df
guri97 <105732963+guri97@users.noreply.github.com>
2022-08-02 14:31:21 +0530

    [gurpreet] UC8_StoringDayAndWageInMap

--- js/EmployeeWageComputation.js
+++ js/EmployeeWageComputation.js
@@ -1,15 +1,9 @@
 console.log("Welcome to the Employee Wage Problem");
 
-///////////////////////////////UC-7///////////////////////////////
-//Use the Daily Wage Array perform following operations using Array Helper Functions
+///////////////////////////////UC-8///////////////////////////////
 /*
-a. Calc total Wage using Array forEach or reduce method
-b. Show the Day along with Daily Wage using Array map helper function
-c. Show Days when Full time wage of 160 were earned using filter function
-d. Find the first occurrence when Full Time Wage was earned using find function
-e. Check if Every Element of Full Time Wage is truly holding Full time wage
-f. Check if there is any Part Time Wage
-g. Find the number of days the Employee Worked
+Store the Day and the Daily Wage along with the Total Wage 
+- Use Map to store Day wise Wage - Compute the total wage using the Map
 */
 
 const IS_FULL_TIME=1;
@@ -22,6 +16,8 @@ let totalEmpWage=0;
 let day=1;  
 let workingHrs=0;
 let empWageArray = new Array();
+let empWageMapObject = new Map();  //UC8
+
 
 function getWorkHours()
 {
@@ -50,6 +46,7 @@ while(day<=MAX_WORKING_DAYS && workingHrs<=MAX_WORKING_HOURS)
     day++
     workingHrs+=empHrs;
     empWageArray.push(empWage)
+    empWageMapObject.set(day,empWage);  //UC8
 }
 console.log("UC4-Total Employee Monthly Wage:"+totalEmpWage);
 console.log("UC5-Total Days:"+(day-1) + " and Total Working Hours:"+workingHrs);
@@ -154,74 +151,81 @@ function GetNumberOfWorkingDay(numOfDays,dailywage)
 }
 console.log("UC7G-Counting Number Of Working Days\n"+empWageArray.reduce(GetNumberOfWorkingDay))
 
+///////////////////////////////UC-8 Store the Day and the Daily Wage along with the Total Wage ///////////////////////////////
 
+console.log("UC-8 Storing Day and Dailywage in Map Object") 
+//For Calculating Total Wage We Already Have A Function,
+//To Use that function we have to Convert map into array,
+//since map is not supoorted with array helper functions
+//To Fetch only values we used empwageobject.values
+console.log("In Map Object\nDay: "+(day-1)+" TotalWage:"+ Array.from(empWageMapObject.values()).reduce(GetTotalWageUsingReduce))
 
 
 /*
------------------------------------------------------------------------------------------------
 Welcome to the Employee Wage Problem
 UC1-Employee is Absent
+-----------------------------------------------------------------------------------------------
 UC2-Employee Wage for day-1 = 0
 UC1-Employee is Absent
 UC2-Employee Wage for day-2 = 0
-UC1-Employee is Present in Part Time
-UC2-Employee Wage for day-3 = 80
+UC1-Employee is Absent
+UC2-Employee Wage for day-3 = 0
 UC1-Employee is Absent
 UC2-Employee Wage for day-4 = 0
+UC1-Employee is Present in Part Time
+UC2-Employee Wage for day-5 = 80
+UC1-Employee is Present in Part Time
+UC2-Employee Wage for day-6 = 80
 UC1-Employee is Present in Full Time
-UC2-Employee Wage for day-5 = 160
-UC1-Employee is Absent
-UC2-Employee Wage for day-6 = 0
-UC1-Employee is Absent
-UC2-Employee Wage for day-7 = 0
+UC2-Employee Wage for day-7 = 160
 UC1-Employee is Absent
 UC2-Employee Wage for day-8 = 0
 UC1-Employee is Present in Part Time
 UC2-Employee Wage for day-9 = 80
-UC1-Employee is Present in Full Time
-UC2-Employee Wage for day-10 = 160
 UC1-Employee is Present in Part Time
-UC2-Employee Wage for day-11 = 80
-UC1-Employee is Present in Part Time
-UC2-Employee Wage for day-12 = 80
-UC1-Employee is Present in Part Time
-UC2-Employee Wage for day-13 = 80
+UC2-Employee Wage for day-10 = 80
+UC1-Employee is Absent
+UC2-Employee Wage for day-11 = 0
+UC1-Employee is Absent
+UC2-Employee Wage for day-12 = 0
+UC1-Employee is Absent
+UC2-Employee Wage for day-13 = 0
 UC1-Employee is Present in Full Time
 UC2-Employee Wage for day-14 = 160
-UC1-Employee is Present in Part Time
-UC2-Employee Wage for day-15 = 80
-UC1-Employee is Present in Part Time
-UC2-Employee Wage for day-16 = 80
+UC1-Employee is Absent
+UC2-Employee Wage for day-15 = 0
 UC1-Employee is Present in Full Time
-UC2-Employee Wage for day-17 = 160
-UC1-Employee is Present in Full Time
-UC2-Employee Wage for day-18 = 160
+UC2-Employee Wage for day-16 = 160
+UC1-Employee is Absent
+UC2-Employee Wage for day-17 = 0
+UC1-Employee is Present in Part Time
+UC2-Employee Wage for day-18 = 80
 UC1-Employee is Absent
 UC2-Employee Wage for day-19 = 0
 UC1-Employee is Present in Full Time
 UC2-Employee Wage for day-20 = 160
 -----------------------------------------------------------------------------------------------
-UC4-Total Employee Monthly Wage:1520
+UC4-Total Employee Monthly Wage:1040
 -----------------------------------------------------------------------------------------------
-UC5-Total Days:20 and Total Working Hours:76
+UC5-Total Days:20 and Total Working Hours:52
 -----------------------------------------------------------------------------------------------
-UC6-EmpWage Array:0,0,80,0,160,0,0,0,80,160,80,80,80,160,80,80,160,160,0,160
+UC6-EmpWage Array:0,0,0,0,80,80,160,0,80,80,0,0,0,160,0,160,0,80,0,160
 -----------------------------------------------------------------------------------------------
 UC7A using Foreach Helper Function
-Total Wage Using Foreach Method:1520
+Total Wage Using Foreach Method:1040
 -----------------------------------------------------------------------------------------------
 UC-7A1 using Reduce Helper Function
-Total Wage Using Reduce Helper Function 1520
+Total Wage Using Reduce Helper Function 1040
 -----------------------------------------------------------------------------------------------
 UC7B using Map Helper Function
-UC7B Display Along Wage with Day using Map Helper Function   
-Day1:0,Day2:0,Day3:80,Day4:0,Day5:160,Day6:0,Day7:0,Day8:0,Day9:80,Day10:160,Day11:80,Day12:80,Day13:80,Day14:160,Day15:80,Day16:80,Day17:160,Day18:160,Day19:0,Day20:160
+UC7B Display Along Wage with Day using Map Helper Function
+Day1:0,Day2:0,Day3:0,Day4:0,Day5:80,Day6:80,Day7:160,Day8:0,Day9:80,Day10:80,Day11:0,Day12:0,Day13:0,Day14:160,Day15:0,Day16:160,Day17:0,Day18:80,Day19:0,Day20:160   
 -----------------------------------------------------------------------------------------------
 UC7C using Filter Helper Function
-UC7C Display FullTimne Alog With DaysDay5:160,Day10:160,Day14:160,Day17:160,Day18:160,Day20:160
+UC7C Display FullTimne Alog With DaysDay7:160,Day14:160,Day16:160,Day20:160
 -----------------------------------------------------------------------------------------------
 UC7D using Find Helper Function
-UC7D First Occurrence When Full Time WageDay5:160
+UC7D First Occurrence When Full Time WageDay7:160
 -----------------------------------------------------------------------------------------------
 UC7E using Every Method Helper Function
 UC7E Checking Every Element Full Time Or Not?
@@ -233,6 +237,10 @@ true
 -----------------------------------------------------------------------------------------------
 UC-7G using Reduce Helper Function
 UC7G-Counting Number Of Working Days
-13
+9
+-----------------------------------------------------------------------------------------------
+UC-8 Storing Day and Dailywage in Map Object
+In Map Object
+Day: 20 TotalWage:1040
 -----------------------------------------------------------------------------------------------
 */
