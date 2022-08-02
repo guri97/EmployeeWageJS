102390a7ec6f9dd1021c3fd6624a8b67bfe69b44
guri97 <105732963+guri97@users.noreply.github.com>
2022-08-02 14:21:52 +0530

    [gurpreet] UC5_CalculateWagesTillMaxDaysAndHoursReached

--- js/EmployeeWageComputation.js
+++ js/EmployeeWageComputation.js
@@ -1,13 +1,16 @@
 console.log("Welcome to the Employee Wage Problem");
 
-///////////////////////////////UC-4///////////////////////////////
-//Calculating Wages for a Month assuming 20 Working Days in a Month
+///////////////////////////////UC-5///////////////////////////////
+//Calculate Wages till a condition of total working hours of 160 or max days of 20 is reached for a month
 const IS_FULL_TIME=1;
 const IS_PART_TIME=2;
-EMP_RATE_PER_HR=20;
+const EMP_RATE_PER_HR=20;
 const MAX_WORKING_DAYS=20;
+const MAX_WORKING_HOURS=160;
 let empHrs =0;
 let totalEmpWage=0;
+let day=1;
+let workingHrs=0;
 
 function getWorkHours()
 {
@@ -27,11 +30,14 @@ function getWorkHours()
             return empHrs;
     }
 }
-for(let day=1;day<=MAX_WORKING_DAYS;day++)
+while(day<=MAX_WORKING_DAYS && workingHrs<=MAX_WORKING_HOURS)
 {
     empCheck = Math.floor(Math.random()*10)%3;
     empWage=EMP_RATE_PER_HR*getWorkHours();
     console.log("UC2-Employee Wage for day-"+day+" = "+empWage);
     totalEmpWage+=empWage;
+    day++
+    workingHrs+=empHrs;
 }
-console.log("UC4-Total Employee Wage:"+totalEmpWage);
+console.log("UC4-Total Employee Monthly Wage:"+totalEmpWage);
+console.log("UC5-Total Days:"+(day-1) + " and Total Working Hours:"+workingHrs);
