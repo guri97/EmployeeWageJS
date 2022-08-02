908d378b62fd15e63078e6098e0690a2ef0a300f
guri97 <105732963+guri97@users.noreply.github.com>
2022-08-02 14:24:27 +0530

    [gurpreet] UC6_StoreDailyWageInArray

--- js/EmployeeWageComputation.js
+++ js/EmployeeWageComputation.js
@@ -1,7 +1,8 @@
 console.log("Welcome to the Employee Wage Problem");
 
-///////////////////////////////UC-5///////////////////////////////
-//Calculate Wages till a condition of total working hours of 160 or max days of 20 is reached for a month
+///////////////////////////////UC-6///////////////////////////////
+//Store the Daily Wage along with the Total Wage 
+//- Save in an Array the Daily Wage
 const IS_FULL_TIME=1;
 const IS_PART_TIME=2;
 const EMP_RATE_PER_HR=20;
@@ -9,8 +10,9 @@ const MAX_WORKING_DAYS=20;
 const MAX_WORKING_HOURS=160;
 let empHrs =0;
 let totalEmpWage=0;
-let day=1;
+let day=1;  
 let workingHrs=0;
+let empWageArray = new Array();
 
 function getWorkHours()
 {
@@ -38,6 +40,56 @@ while(day<=MAX_WORKING_DAYS && workingHrs<=MAX_WORKING_HOURS)
     totalEmpWage+=empWage;
     day++
     workingHrs+=empHrs;
+    empWageArray.push(empWage)
 }
 console.log("UC4-Total Employee Monthly Wage:"+totalEmpWage);
 console.log("UC5-Total Days:"+(day-1) + " and Total Working Hours:"+workingHrs);
+console.log("UC6-EmpWage Array:"+empWageArray)
+
+
+/*
+Welcome to the Employee Wage Problem
+UC1-Employee is Present in Part Time
+UC2-Employee Wage for day-1 = 80
+UC1-Employee is Present in Full Time
+UC2-Employee Wage for day-2 = 160
+UC1-Employee is Present in Full Time
+UC2-Employee Wage for day-3 = 160
+UC1-Employee is Present in Part Time
+UC2-Employee Wage for day-4 = 80
+UC1-Employee is Absent
+UC2-Employee Wage for day-5 = 0
+UC1-Employee is Absent
+UC2-Employee Wage for day-6 = 0
+UC1-Employee is Present in Part Time
+UC2-Employee Wage for day-7 = 80
+UC1-Employee is Absent
+UC2-Employee Wage for day-8 = 0
+UC1-Employee is Present in Full Time
+UC2-Employee Wage for day-9 = 160
+UC1-Employee is Present in Full Time
+UC2-Employee Wage for day-10 = 160
+UC1-Employee is Absent
+UC2-Employee Wage for day-11 = 0
+UC1-Employee is Present in Part Time
+UC2-Employee Wage for day-12 = 80
+UC1-Employee is Present in Full Time
+UC2-Employee Wage for day-13 = 160
+UC1-Employee is Present in Part Time
+UC2-Employee Wage for day-14 = 80
+UC1-Employee is Present in Part Time
+UC2-Employee Wage for day-15 = 80
+UC1-Employee is Present in Full Time
+UC2-Employee Wage for day-16 = 160
+UC1-Employee is Present in Part Time
+UC2-Employee Wage for day-17 = 80
+UC1-Employee is Present in Full Time
+UC2-Employee Wage for day-18 = 160
+UC1-Employee is Present in Full Time
+UC2-Employee Wage for day-19 = 160
+UC1-Employee is Absent
+UC2-Employee Wage for day-20 = 0
+UC4-Total Employee Monthly Wage:1840
+UC5-Total Days:20 and Total Working Hours:92
+UC6-EmpWage Array:80,160,160,80,0,0,80,0,160,160,0,80,160,80,80,160,80,160,160,0
+*/
